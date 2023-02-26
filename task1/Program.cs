d22953aeb400ce793684f9263be4fe389704ad2a
Alexandr-Goncharov34 <alexandr-goncharov34@mail.ru>
2023-02-26 19:17:45 +0300

    sem1
    sem1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int max = 0;
+int min = 0;
+
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "" ) ;
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "" ) ;
+
+
+if (firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
